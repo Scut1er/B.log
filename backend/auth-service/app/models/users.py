57304d153de8a4c28a8dd05a38bb71d7382bed4f
Scut1er <lyashenkostefan@gmail.com>
2025-01-13 20:55:36 +0300

    corrected users model, fixed depricated time-format, added Docker-container for auth-service.

--- backend/auth-service/app/models/users.py
+++ backend/auth-service/app/models/users.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timezone
 from typing import Optional
 
 from sqlalchemy import ForeignKey, TIMESTAMP
@@ -16,9 +16,7 @@ class Users(Base):
     password_hash: Mapped[str]
     fullname: Mapped[Optional[str]]
     is_verified: Mapped[bool] = mapped_column(default=False)
-    verification_token: Mapped[Optional[str]]
-    verification_token_expires_at: Mapped[datetime] = mapped_column(TIMESTAMP)
-    created_at: Mapped[datetime] = mapped_column(default=datetime.utcnow)
+    created_at: Mapped[datetime] = mapped_column(default=lambda: datetime.now(timezone.utc))
 
     role_id: Mapped[int] = mapped_column(ForeignKey("roles.id"))
 
--- backend/auth-service/app/utils/repository.py
+++ backend/auth-service/app/utils/repository.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 
-from sqlalchemy import insert, select
+from sqlalchemy import insert, select, update
 
 from app.db.db import async_session_maker
 
@@ -26,6 +26,13 @@ class SQLAlchemyRepository(AbstractRepository):
             await session.commit()
             return result.scalar_one()
 
+    async def find_by_id(self, record_id: int):
+        """Получение записи по id"""
+        async with async_session_maker() as session:
+            stmt = select(self.model).where(self.model.id == record_id)
+            result = await session.execute(stmt)
+            return result.scalar_one_or_none()
+
     async def find_all(self):
         """Получение всех записей"""
         async with async_session_maker() as session:
@@ -33,3 +40,13 @@ class SQLAlchemyRepository(AbstractRepository):
             result = await session.execute(stmt)
             result = [row[0].to_read_model() for row in result.scalars()]
             return result
+
+    async def update(self, record_id: int, data: dict) -> bool:
+        """Обновление записи"""
+        async with async_session_maker() as session:
+            stmt = (update(self.model).
+                    where(self.model.id == record_id).
+                    values(**data))
+            result = await session.execute(stmt)
+            await session.commit()
+        return result.rowcount  # кол-во измененных строк в бд
